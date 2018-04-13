51956c6f47a356bba2d987b660fb36413d1ee068
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-13 00:46:18 -0400

    Added UPDATE_QUOTE action reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,9 @@
 import {combineReducers} from 'redux';
 import {QUOTES_AVAILABLE, ADD_QUOTE, UPDATE_QUOTE, DELETE_QUOTE} from "../actions/";
 
-let dataState = { quotes: [], loading:true};
+let dataState = {quotes: [], loading: true};
 
-const dataReducer = ( state = dataState, action) => {
+const dataReducer = (state = dataState, action) => {
     switch (action.type) {
         case ADD_QUOTE: {
             let quotes = cloneObject(state.quotes);
@@ -13,9 +13,22 @@ const dataReducer = ( state = dataState, action) => {
         }
 
         case QUOTES_AVAILABLE: {
-            state = Object.assign({}, state, {quotes: action.quotes, loading:false});
+            state = Object.assign({}, state, {quotes: action.quotes, loading: false});
             return state;
         }
+
+        case UPDATE_QUOTE: {
+            let quote = action.quote;
+            let quotes = cloneObject(state.quotes);
+            let index = getIndex(quotes, quote.id);
+            if (index !== -1) {
+                quotes[index]['author'] = quote.author;
+                quotes[index]['text'] = quote.text;
+            }
+            state = Object.assign({}, state, { quotes: quotes});
+            return state;
+        }
+        
         default:
             return state;
     }
