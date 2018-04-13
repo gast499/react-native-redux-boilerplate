ba767dfe630ede76fbbbacaa6e0ed7c138f591dd
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-13 00:42:47 -0400

    Added QUOTES_AVAILABLE action reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -11,6 +11,11 @@ const dataReducer = ( state = dataState, action) => {
             state = Object.assign({}, state, {quotes: quotes});
             return state;
         }
+
+        case QUOTES_AVAILABLE: {
+            state = Object.assign({}, state, {quotes: action.quotes, loading:false});
+            return state;
+        }
         default:
             return state;
     }
