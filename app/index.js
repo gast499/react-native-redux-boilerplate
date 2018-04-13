2edec7b314707ec9fe3bb44f4776f19ea828b323
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-13 00:08:28 -0400

    Created ComponentDidMount function

--- app/index.js
+++ app/index.js
@@ -8,5 +8,14 @@ import {connect} from 'react-redux';
 import { getQuotes } from './actions'
 
 class Main extends Component {
-
+    componentDidMount(){
+        var _this = this;
+        AsyncStorage.getItem('data', (err,data) =>{
+            if (data === null){
+                AsyncStorage.setItem('data', JSON.stringify(Data.quotes));
+                _this.props.getQuotes();
+            }
+        });
+    }
+    
 }
